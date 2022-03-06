5b98aae638abea1d6039b0686a5795e7646a199e
MarcoBeltran97 <marcobeltran97@gmail.com>
2022-03-06 00:48:31 -0500

    Se implementa el proyecto

---
+++ Gimnasio/src/test/java/com/init/gimnasio/GimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
